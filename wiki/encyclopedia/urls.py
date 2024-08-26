00f081f2de18c30b7bcfd9d16e02bfbcb9f523d1
AlaeC <149951671+AlaeC@users.noreply.github.com>
2024-08-26 11:53:56 +0100

    Delete wiki directory

--- wiki/encyclopedia/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("<url_name>/", views.find_page, name="entry"),
-    path("random", views.random_page, name="random")
-]
